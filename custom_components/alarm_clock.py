9a56ae34e14bb3537d855ac18ef50df411fc2f38
Ben Origas <borigas@gmail.com>
2016-07-14 00:32:51 +0000

    Got toggle on alarm clock showing

--- custom_components/alarm_clock.py
+++ custom_components/alarm_clock.py
@@ -1,7 +1,7 @@
 import logging
 from homeassistant.helpers.entity import ToggleEntity
 from homeassistant.helpers.entity_component import EntityComponent
-
+from homeassistant.const import (ATTR_ENTITY_ID, SERVICE_TURN_OFF, SERVICE_TURN_ON, STATE_ON)
 
 DOMAIN = 'alarm_clock'
 
@@ -9,9 +9,31 @@ ENTITY_ID_FORMAT = DOMAIN + '.{}'
 
 _LOGGER = logging.getLogger(__name__)
 
-#def setup_platform(hass, config, add_devices, discovery_info=None):
+def is_on(hass, entity_id):
+	return hass.states.is_state(entity_id, STATE_ON)
+
+def turn_on(hass, entity_id):
+	hass.services.call(DOMAIN, SERVICE_TURN_ON, {ATTR_ENTITY_ID: entity_id})
+
+def turn_off(hass, entity_id):
+	hass.services.call(DOMAIN, SERVICE_TURN_OFF, {ATTR_ENTITY_ID: entity_id})
+
+
 def setup(hass, config):
 	_LOGGER.info('Setting up ' + DOMAIN)
+
+	def toggle_service(service):
+		target_inputs = component.extract_from_service(service)
+		
+		for input_b in target_inputs:
+			if service.service == SERVICE_TURN_ON:
+				input_b.turn_on()
+			else:
+				input_b.turn_off()
+
+	hass.services.register(DOMAIN, SERVICE_TURN_OFF, toggle_service)
+	hass.services.register(DOMAIN, SERVICE_TURN_ON, toggle_service)
+	
 
 	component = EntityComponent(_LOGGER, DOMAIN, hass)
 
@@ -19,7 +41,6 @@ def setup(hass, config):
 
 	_LOGGER.info('Created ' + DOMAIN + ' instance')
 
-#	add_devices([clock])
 	component.add_entities([clock])
 
 	_LOGGER.info('Added ' + DOMAIN + ' devices')
