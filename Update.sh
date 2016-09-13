78f5f4cc0cd607fa6ae57caed349bf5db06d951c
Ben Origas <borigas@gmail.com>
2016-09-13 18:03:19 +0000

    Modify update script

--- Update.sh
+++ Update.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Stop HASS
-sudo systemctl stop home-assistant@hass
+sudo systemctl stop home-assistant.service
 # Become user 'hass'
 sudo su -s /bin/bash hass <<'EOF'
 # Activate the virtualenv
-source /srv/hass/bin/activate
+source /srv/hass/hass_venv/bin/activate
 # Install Home Assistant
 pip3 install --upgrade homeassistant
 exit
 EOF
 
 # Restart Home Assistant
-sudo systemctl restart home-assistant@hass
+sudo systemctl restart home-assistant.service
